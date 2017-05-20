46663cf889dc4f11bf6a8a1dbcd1de1c0862bbdf
Francis Miyoba <francismiyoba@gmail.com>
2017-05-20 02:06:00 -0700

    Add middleware for finding a book by id

--- Routes/bookRouter.js
+++ Routes/bookRouter.js
@@ -29,30 +29,38 @@ const routes=(Book)=>{
             });
           });
 
+          //middleware to find a single book
+bookRouter.use('/:bookId',(req,res)=>{
+        Book.findById(req.params.bookId,(err,book)=>{
+          if(err)
+            res.status(500).send(err);
+          else if(book)
+          {
+            //we found the book so we update item
+            req.book=book;
+            next();//continue to others
+          }
+          else
+          {
+            res.send(404).send('Book not found');
+          }
+        });
+});
+
   bookRouter.route('/Books/:bookId')
             .get((req,res)=>{
-              Book.findById(req.params.bookId,(err,book)=>{
-                if(err)
-                  res.status(500).send(err);
-                res.json(book);
-              });
+              //the middleware will handle this
+            res.json(req.book);
             })
             .put((req,res)=>{
               //put will just replce an existing item
-              Book.findById(req.params.bookId,(err,book)=>{
-                if(err)
-                  res.status(500).send(err);
-                  else
-                  {
-                    title=req.body.title;
-                    genre=req.body.genre;
-                    author=req.body.author;
-                    read=req.body.read;
-                    //save the new book to db 
-                    book.save();
-                    res.json(book);
-                  }
-              })
+              req.book.title=req.body.title;
+              req.book.genre=req.body.genre;
+              req.book.author=req.body.author;
+              req.book.read=req.body.author;
+
+              req.book.save();
+              res.json(req.book);
             })
       
 
